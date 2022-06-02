1c44e1af2837d5295348b50928c5ab943dc22a53
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-06-02 15:52:29 +0200

    non c'est celui la qui marchegit add src/components/Condition.vue !

--- src/components/Condition.vue
+++ src/components/Condition.vue
@@ -1,24 +1,23 @@
 <script setup>
 import {ref} from "vue"
-let a = false;
 
-const user = ref({id: 0, name:"Hery", log:a})
+
+const user = ref(null)
 
 function Connexion(){
- a = true
- console.log(a)
+user.value = {id: 0, name:"Hery"}
+ 
 }
 
 function Deconnexion(){
- a = false
- console.log(a)
+user.value = null
 }
 </script>
 
 <template>
 <h1>CONDITION</h1>
-<h2 v-if="user.log">Hello {{user.name}}</h2>
+<h2 v-if="user">Hello {{user.name}}</h2>
 <h2 v-else>Hello Anonyme</h2>
-<button v-if="user.log" @click="Deconnexion()">se déconnecter</button>
+<button v-if="user" @click="Deconnexion()">se déconnecter</button>
 <button v-else @click="Connexion()">se connecter</button>
 </template>
