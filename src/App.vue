db5b2392a9175f8176aed5ee720d87188b9f3341
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-06-02 14:06:13 +0200

    page formulaire

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Helloworld from "@/components/Helloworld.vue"
+import Formulaire from "./components/Formulaire.vue";
 </script>
 
 <template>
 <h1>blabla</h1>
 <Helloworld/>
+<Formulaire/>
 </template>
---
+++ src/components/Formulaire.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue"
+
+const tableau = ref([1,2,3])
+
+const listObj = ref([
+    {id: 0, name:"Alice"},
+    {id: 1, name:"Alice"},
+    {id: 2, name:"Alice"}
+])
+</script>
+<template>
+    <div>
+    <p v-for="i in tableau"> {{i}} </p>
+    <ul>
+        <li v-for="monObjet in listObj" :key="monObjet.id" >{{monObjet}}</li>
+    </ul>
+    <div v-for="monObjet in listObj" :key="monObjet.id" >
+    <h2>{{monObjet.name}} </h2>
+    <p>{{monObjet.id}}</p>
+    </div>
+</div>
+
+</template>
