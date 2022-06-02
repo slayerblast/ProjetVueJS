731fe3d210f0b8fbc8caef10e9b45e6ad59e3fd0
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-06-02 09:54:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from "vue"
+const cle = ref(0)
+setInterval(()=>{
+  cle.value++
+}, 1000)
+function decryptCle(){
+  cle.value = 0;
+}
+</script>
+
+<template>
+<h1>DECRYPTEUR</h1>
+<button @click="decryptCle()">mettez la clé</button>
+<input type="number" v-model="cle">
+<p>{{cle}}</p>
+</template>
