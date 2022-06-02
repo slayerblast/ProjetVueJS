0148decf4a67ef9a50ebf592e9ab64d5bea11860
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-06-02 14:57:25 +0200

    page condition mais la fonction sur les boutons ne marche pas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Helloworld from "@/components/Helloworld.vue"
 import Formulaire from "./components/Formulaire.vue";
+import Condition from "./components/Condition.vue";
 </script>
 
 <template>
 <h1>blabla</h1>
 <Helloworld/>
+<hr>
 <Formulaire/>
+<hr>
+<Condition/>
+
 </template>
---
+++ src/components/Condition.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue"
+let a = false;
+
+const user = ref({id: 0, name:"Hery", log:a})
+
+function Connexion(){
+ a = true
+ console.log(a)
+}
+
+function Deconnexion(){
+ a = false
+ console.log(a)
+}
+</script>
+
+<template>
+<h1>CONDITION</h1>
+<h2 v-if="user.log">Hello {{user.name}}</h2>
+<h2 v-else>Hello Anonyme</h2>
+<button v-if="user.log" @click="Deconnexion()">se déconnecter</button>
+<button v-else @click="Connexion()">se connecter</button>
+</template>
